3d29682e6838f5ee6090f8ab2aef4761553bca1d
lekshmikjayan <lekshmikjayan27@gmail.com>
2022-06-18 18:36:55 +0530

    First Commit
    Pushing HTML programs

---
+++ js sample.js
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+
+<html>
+<head>
+	<title>JS</title>
+</head>
+
+<body>
+<script type = "text/Javascript">
+
+document.write ("Hello World");
+
+</script>
+</body>
+</html>
